5b51071046f082359683e04691c3fff42862ceb6
magisk317 <93979778+magisk317@users.noreply.github.com>
2025-12-19 00:49:14 +0800

    chore: remove deprecated gateway packages

--- packages/gateway-protocol/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './frames.js';
-export * from './events.js';
-export * from './actions.js';
-export * from './segments.js';
-export * from './errors.js';
--- packages/gateway-sdk/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from '@naplink/napgram-gateway-client';
